91f2f3facae5cff584b207cc4c1abf3eb4ad8923
Eduardo <eazevedo.016@gmail.com>
2022-07-19 12:15:45 -0400

    finalizando sintatico, mas ainda esta estatico a leitura de tokens. Precisa fazer cconsumir os tokens do lexico.

--- Sintatico.py
+++ Sintatico.py
@@ -1,87 +1,5 @@
-
-def main():
-    global pilha 
-    global cadeia
-
-    pilha = 'E$'
-    cadeia = ''
-
-    token = input()
-    while not(pilha == '$'):
-
-        # PILHA COM INICIAL NÃO TERMINAL
-
-        if (pilha.startswith("E")):
-            isE(token)
-        elif (pilha.startswith("E'")):
-            isElinha(token)
-        elif (pilha.startswith("T")):
-            isT(token)
-        elif (pilha.startswith("T'")):
-            isTlinha(token)
-        elif (pilha.startswith("P")):
-            isP(token)
-        elif(pilha.startswith("P'")):
-            isPlinha(token)
-        elif(pilha.startswith("F")):
-            isF(token)
-
-        # PILHA COM INICIAL TERMINAL
-
-        elif(pilha.startswith("id")):
-            pilha = pilha.replace("id","", 1)
-            cadeia = cadeia + 'id'
-            break
-        elif(pilha.startswith("+")):
-            pilha = pilha.replace("+","", 1)
-            cadeia = cadeia + '+'
-        elif(pilha.startswith("-")):
-            pilha = pilha.replace("-","", 1)
-            cadeia = cadeia + '-'
-        elif(pilha.startswith("/")):
-            pilha = pilha.replace("/","", 1)
-            cadeia = cadeia + '/'
-            break
-        elif(pilha.startswith("*")):
-            pilha = pilha.replace("*","", 1)
-            cadeia = cadeia + '*'
-            break
-        elif(pilha.startswith("^")):
-            pilha = pilha.replace("^","", 1)
-            cadeia = cadeia + '^'
-            break
-        elif(pilha.startswith("exp")):
-            pilha = pilha.replace("exp","", 1)
-            cadeia = cadeia + 'exp'
-            break
-        elif(pilha.startswith("(")):
-            pilha = pilha.replace("(","", 1)
-            cadeia = cadeia + '('
-            break
-        elif(pilha.startswith(")")):
-            pilha = pilha.replace(")","", 1)
-            cadeia = cadeia + ')'
-            break
-        elif(pilha.startswith("[")):
-            pilha = pilha.replace("[","", 1)
-            cadeia = cadeia + '['
-            break
-        elif(pilha.startswith("]")):
-            pilha = pilha.replace("]","", 1)
-            cadeia = cadeia + ']'
-            break
-        elif(pilha.startswith("$")):
-            pilha = pilha.replace("$","", 1)
-            cadeia = cadeia + '$'
-            break       
-        
-
-
-
 def isE(token):
-
     global pilha 
-
     if(token=='id'):
         pilha = pilha.replace("E","TE'",1)
     elif(token=='exp'):
@@ -91,29 +9,23 @@ def isE(token):
     else:
         raise Exception('Erro sintático.')
 
-    
+        
 def isElinha(token):
-
     global pilha 
-
     if(token == '+'):
         pilha = pilha.replace("E'","+TE'",1)
     elif(token == '-'):
         pilha = pilha.replace("E'","-TE'",1)
     elif(token == ')'):
-        pilha = pilha.replace("E'","e",1)
+        pilha = pilha.replace("E'","",1)
     elif(token == '$'):
-        pilha = pilha.replace("E'","e",1)
+        pilha = pilha.replace("E'","",1)
     else:
         raise Exception('Erro sintático.')
 
 
-
-
 def isT(token): 
-
     global pilha 
-
     if(token=='id'):
         pilha = pilha.replace("T","PT'",1)
     elif(token=='exp'):
@@ -123,12 +35,11 @@ def isT(token):
     else:
         raise Exception('Erro sintático.') 
 
+
 def isTlinha(token):
-
     global pilha 
-
     if(token=='+' or  token=='-' or  token==')' or  token=='$'):
-        pilha = pilha.replace("T'","e",1)
+        pilha = pilha.replace("T'","",1)
     elif(token=='/'):
         pilha = pilha.replace("T'","/PT'",1)
     elif(token=='*'):
@@ -138,9 +49,7 @@ def isTlinha(token):
     
 
 def isP(token):
-
     global pilha 
-
     if(token=='id' or  token=='('):
         pilha = pilha.replace("P","FP'",1)
     elif(token=='exp'):
@@ -148,30 +57,117 @@ def isP(token):
     else:
         raise Exception('Erro sintático.') 
 
+
 def isPlinha(token):
-
     global pilha 
-
     if(token=='+' or  token=='-' or  token=='/' or  token=='*' or  token==')' or  token=='$'):
-        pilha = pilha.replace("P'","e",1)
+        pilha = pilha.replace("P'","",1)
     elif(token=='^'):
         pilha = pilha.replace("P'","^FP'",1)
     else:
         raise Exception('Erro sintático.') 
 
+
 def isF(token):
-
     global pilha 
-
     if(token=='id'):
         pilha = pilha.replace("F", "id", 1)
     elif(token=='('):
         pilha = pilha.replace("F", "(E)", 1)
     else:
-        raise Exception('Erro sintático.') 
+        raise Exception('Erro sintático.')
 
+
+def Sintatico(tokenList):
+
+    global pilha 
+    global cadeia
+
+    pilha = 'E$'
+    cadeia = ''
+
+    for token in tokenList:
+        if ((pilha == '$') and (token == '$')):
+            break
+        tokenList.append("$")
+        while not((pilha == '$') and (token == '$')):
+
+            # PILHA COM INICIAL NÃO TERMINAL
+            if (pilha.startswith("E") and pilha[1] != "'"):
+                isE(token)
+            elif (pilha.startswith("E'")):
+                isElinha(token)
+            elif (pilha.startswith("T") and pilha[1] != "'"):
+                isT(token)
+            elif (pilha.startswith("T'")):
+                isTlinha(token)
+            elif (pilha.startswith("P") and pilha[1] != "'"):
+                isP(token)
+            elif(pilha.startswith("P'")):
+                isPlinha(token)
+            elif(pilha.startswith("F")):
+                isF(token)
+
+            # PILHA COM INICIAL TERMINAL
+
+            elif(pilha.startswith("id")):
+                pilha = pilha.replace("id","", 1)
+                cadeia = cadeia + 'id'
+                break
+            elif(pilha.startswith("+")):
+                pilha = pilha.replace("+","", 1)
+                cadeia = cadeia + '+'
+                break
+            elif(pilha.startswith("-")):
+                pilha = pilha.replace("-","", 1)
+                cadeia = cadeia + '-'
+                break
+            elif(pilha.startswith("/")):
+                pilha = pilha.replace("/","", 1)
+                cadeia = cadeia + '/'
+                break
+            elif(pilha.startswith("*")):
+                pilha = pilha.replace("*","", 1)
+                cadeia = cadeia + '*'
+                break
+            elif(pilha.startswith("^")):
+                pilha = pilha.replace("^","", 1)
+                cadeia = cadeia + '^'
+                break
+            elif(pilha.startswith("exp")):
+                pilha = pilha.replace("exp","", 1)
+                cadeia = cadeia + 'exp'
+                break
+            elif(pilha.startswith("(")):
+                pilha = pilha.replace("(","", 1)
+                cadeia = cadeia + '('
+                break
+            elif(pilha.startswith(")")):
+                pilha = pilha.replace(")","", 1)
+                cadeia = cadeia + ')'
+                break
+            elif(pilha.startswith("[")):
+                pilha = pilha.replace("[","", 1)
+                cadeia = cadeia + '['
+                break
+            elif(pilha.startswith("]")):
+                pilha = pilha.replace("]","", 1)
+                cadeia = cadeia + ']'
+                break
+            elif(pilha.startswith("$")):
+                pilha = pilha.replace("$","", 1)
+                cadeia = cadeia + '$'
+                break   
+
+
+    
    
+def main():
 
+    tokenList = ['id','id','*','id', '^', 'id']
+    Sintatico(tokenList)
+    print("Cadeia aceita")
+    
 main()
 
 
